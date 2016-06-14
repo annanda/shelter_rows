64f5435af1142f424e74698afdf8b86aa0898f59
Annanda Sousa <annanda.sousa@gmail.com>
2016-06-13 22:23:39 -0300

    Adicionando mais dados

--- analyze.py
+++ analyze.py
@@ -5,10 +5,15 @@ import rows
 table_3 = rows.import_from_csv('novo.csv')
 
 quantidade_de_exemplos = len(table_3)
-print '--------------- Início da Análise -----------------------\n'
-print 'Quantidade de exemplos: {}'.format(quantidade_de_exemplos)
-
 cats = filter(lambda row: row.animaltype == "Cat", table_3)
 dogs = filter(lambda row: row.animaltype == "Dog", table_3)
+male = filter(lambda row: row.sex == "Male", table_3)
+female = filter(lambda row: row.sex == "Female", table_3)
+sex_unknown = filter(lambda row: row.sex == "Unknown", table_3)
 
+
+print '--------------- Início da Análise -----------------------\n'
+print 'Quantidade de exemplos: {}'.format(quantidade_de_exemplos)
 print 'Quantidade de Gatos vs Quantidade de Cachorros: {} vs {}'.format(len(cats), len(dogs))
+print 'Quantidade de Machos vs Quantidade de Fêmeas: {} vs {}'.format(len(male), len(female))
+print 'Quantidade de animais com sexo desconecido: {}'.format(len(sex_unknown))
