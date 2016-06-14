0128ecd7e6d1d20048e5d0e587d7de85b1cc81e0
Annanda Sousa <annanda.sousa@gmail.com>
2016-06-13 22:07:28 -0300

    Começando a análise

--- analyze.py
+++ analyze.py
@@ -1,53 +1,14 @@
+# coding: utf-8
+
 import rows
-from collections import OrderedDict
 
+table_3 = rows.import_from_csv('novo.csv')
 
-def get_animal_age(name):
-    if name == '': return -1
+quantidade_de_exemplos = len(table_3)
+print '--------------- Início da Análise -----------------------\n'
+print 'Quantidade de exemplos: {}'.format(quantidade_de_exemplos)
 
-    how_many, period = name.split()
+cats = filter(lambda row: row.animaltype == "Cat", table_3)
+dogs = filter(lambda row: row.animaltype == "Dog", table_3)
 
-    how_many = int(how_many)
-    # remove plural
-    period = period[:-1] if period.endswith('s') else period
-
-    daysInPeriod = {
-        'year': 365,
-        'month': 31,
-        'week': 7,
-        'day': 1
-    }.get(period)
-
-    return how_many * daysInPeriod
-
-table_1 = rows.import_from_csv("train.csv")
-
-new_fields = OrderedDict([
-    ('animalid', rows.fields.UnicodeField),
-    ('name', rows.fields.UnicodeField),
-    ('datetime', rows.fields.DatetimeField),
-    ('outcometype', rows.fields.UnicodeField),
-    ('outcomesubtype', rows.fields.UnicodeField),
-    ('animaltype', rows.fields.UnicodeField),
-    ('sex', rows.fields.UnicodeField),
-    ('castration', rows.fields.UnicodeField),
-    ('ageuponoutcome', rows.fields.UnicodeField),
-    ('breed', rows.fields.UnicodeField),
-    ('color', rows.fields.UnicodeField)
-])
-
-table_2 = rows.Table(fields=new_fields)
-for row in table_1:
-    if row.sexuponoutcome and row.sexuponoutcome != u'Unknown':
-        castration, sex = row.sexuponoutcome.split()
-    else:
-        sex, castration = u'Unknown', u'Unknown'
-    age_in_days = get_animal_age(row.ageuponoutcome)
-    table_2.append({'animalid': row.animalid, 'name': row.name, 'datetime': row.datetime,
-                    'outcometype': row.outcometype, 'outcomesubtype': row.outcomesubtype,
-                    'animaltype': row.animaltype, 'sex': sex, 'castration': castration,
-                    'ageuponoutcome': age_in_days, 'breed': row.breed, 'color': row.color})
-
-rows.export_to_csv(table_2, "novo.csv")
-
-
+print 'Quantidade de Gatos vs Quantidade de Cachorros: {} vs {}'.format(len(cats), len(dogs))
