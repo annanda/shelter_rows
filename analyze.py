64ad830c3e302a6d3d06584c6d095bd388b729ee
Annanda Sousa <annanda.sousa@gmail.com>
2016-06-18 22:38:58 -0300

    Melhorando a formatação da saída

--- analyze.py
+++ analyze.py
@@ -35,22 +35,29 @@ holidays_and_weekend = filter(lambda row: row.holiday == "True" and (row.datetim
 
 print '--------------- Início da Análise -----------------------\n'
 print 'Quantidade de exemplos: {}'.format(quantidade_de_exemplos)
+print '======================================================================================================'
+print '======================================================================================================'
 print 'Quantidade de Gatos vs Quantidade de Cachorros: {} vs {}'.format(len(cats), len(dogs))
+print '======================================================================================================'
 print 'Quantidade de Machos vs Quantidade de Fêmeas: {} vs {}'.format(len(male), len(female))
 print 'Quantidade de animais com sexo desconecido: {}'.format(len(sex_unknown))
+print '======================================================================================================'
 print 'Quantidade de animais devolvidos para o dono: {} --> {:.4g}%'.format(len(return_to_owner), len(return_to_owner)/quantidade_de_exemplos *100)
 print 'Quantidade de animais que sofreram eutanásia: {} --> {:.4g}%'.format(len(euthanasia), len(euthanasia)/quantidade_de_exemplos *100)
 print 'Quantidade de animais que foram adotados: {} --> {:.4g}%'.format(len(adoption), len(adoption)/quantidade_de_exemplos *100)
 print 'Quantidade de animais que foram transferidos: {} --> {:.4g}%'.format(len(transfer), len(transfer)/quantidade_de_exemplos *100)
 print 'Quantidade de animais que morreram: {} --> {:.4g}%'.format(len(died), len(died)/quantidade_de_exemplos *100)
+print '======================================================================================================'
 print 'Quantidade de animais que castrados vs não castrados: {} vs {}'.format(len(castrated), len(not_castrated))
-print 'Quantidade de animais com informação de castração desconecida: {}'.format(len(castration_unknown))
+print 'Quantidade de animais com informação de castração desconhecida: {}'.format(len(castration_unknown))
+print '======================================================================================================'
 print 'Quantidade de animais com nome: {}'.format(len(has_name))
 print '{:.4g}% dos adotados tinham nome. Quantidade: {}'.format(len(has_name_and_adoption)/len(adoption) *100, len(has_name_and_adoption))
-print '{:.4g}% dos animais adotados, tinham nome'.format(len(has_name_and_adoption)/len(adoption) *100)
 print '{:.4g}% dos animais com nome, foram adotados'.format(len(has_name_and_adoption)/len(has_name) *100)
+print '======================================================================================================'
 print 'Quantidade de animais com datetime em final de semana: {}  --> {:.4g}%'.format(len(weekend), len(weekend)/quantidade_de_exemplos *100)
 print 'Quantidade de animais com datetime em final de semana e foi adotado: {}  --> {:.4g}%'.format(len(weekend_and_adoption), len(weekend_and_adoption)/len(adoption) *100)
+print '======================================================================================================'
 print 'Quantidade de animais com datetime em feriado: {} '.format(len(holidays))
 print 'Quantidade de animais com datetime em feriado e foi adotado: {}  --> {:.4g}%'.format(len(holidays_and_adoption), len(holidays)/len(adoption) *100)
 print 'Quantidade de animais com datetime em feriado e final de semana: {}'.format(len(holidays_and_weekend))
